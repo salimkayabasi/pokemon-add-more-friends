d536e8555037d9423e915baeae25dc15b8c47fa0
Salim KAYABASI <salim.kayabasi@gmail.com>
2020-02-08 21:02:08 +0100

    adding pokemongofriendcodes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 
-const POKEMON_GO_URL = 'https://www.fcswap.com/game/pokemon-go/';
 const {POKEMON_CODE, CI = false} = process.env;
+const FCSWAP_URL = 'https://www.fcswap.com/game/pokemon-go/';
+const PGFC_URL = 'https://www.pokemongofriendcodes.com/'
 
 const IS_CI = !!CI;
 
@@ -10,14 +11,29 @@ process.on('unhandledRejection', error => {
     process.exit(error.code || 99);
 });
 
-(async () => {
+const getPage = async (url) => {
     const browser = await puppeteer.launch({
         headless: IS_CI,
         defaultViewport: null
     });
     const page = await browser.newPage();
-    await page.goto(POKEMON_GO_URL);
+    await page.goto(url);
+    return {browser, page};
+};
+
+const addToFCSWAP = async () => {
+    const {browser, page} = await getPage(FCSWAP_URL);
     await page.type('#codetext', POKEMON_CODE, {delay: 100});
-    await page.$eval('input[name="submit"]', submit => submit.click());
+    await page.$eval('input[name="submit"]', button => button.click());
     await browser.close();
+};
+const addToPGFC = async () => {
+    const {browser, page} = await getPage(PGFC_URL);
+    await page.type('#inputFriendCode', POKEMON_CODE, {delay: 100});
+    await page.$eval('#inputFcForm button', button => button.click());
+    await browser.close();
+};
+(async () => {
+    await addToFCSWAP();
+    await addToPGFC();
 })();
