c2a2c3671e3c062c689f7fe0191a780c7af4d0f3
Salim KAYABASI <salim.kayabasi@gmail.com>
2020-02-08 20:43:52 +0100

    fixing CI issue

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const puppeteer = require('puppeteer');
 
 const POKEMON_GO_URL = 'https://www.fcswap.com/game/pokemon-go/';
-const {POKEMON_CODE} = process.env;
+const { POKEMON_CODE } = process.env;
 
-const IS_CI = !!(process.env.CI)
+const IS_CI = !!(process.env.CI);
+
+process.on('unhandledRejection', error => {
+    process.exit(error.code || 99);
+});
 
 (async () => {
     const browser = await puppeteer.launch({
-        headless: !IS_CI,
+        headless: IS_CI,
         defaultViewport: null
     });
     const page = await browser.newPage();
